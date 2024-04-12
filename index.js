85a7ccbec4a807c834e1bd9a2898cca771004527
Sharon <Sharonaquilla28@gmail.com>
2024-04-12 11:28:12 +0300

    adds fetch lab

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
-function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
+ function fetchBooks() {
+    // To pass the tests, don't forget to return your fetch!
+    return fetch("https://anapioficeandfire.com/api/books") //returning a response that can be converted into json. Without the return this won't work
+    .then(res => res.json()) //parsing the json string into a JS object
+    .then((json) => renderBooks(json)); //calling the renderBooks function to take place after the json has been parsed
+  }
   
-}
+  function renderBooks(books) {
+    const main = document.querySelector('main');
+    books.forEach(book => {
+      const h2 = document.createElement('h2');
+      h2.innerHTML = book.name;
+      main.appendChild(h2);
+    });
+  }
+  
+  document.addEventListener('DOMContentLoaded', function() {
+    fetchBooks();
+  });
+  
+  
 
 function renderBooks(books) {
   const main = document.querySelector('main');
